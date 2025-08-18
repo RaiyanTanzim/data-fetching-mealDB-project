e03f916bb57762f5a1bbf9716d576c10e197bf05
RaiyanTanzim <writetoraiyan@gmail.com>
2025-08-19 02:29:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+//https://www.themealdb.com/api/json/v1/1/search.php?s=
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
+import "./App.css";
+
+function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+      .then((response) => response.json())
+      .then((data) => {
+        setMeals(data.meals);
+      });
+  }, []);
+  return (
+    <>
+      <div className="min-h-screen bg-grey-100">
+        <div className="bg-blue-500 py-10 text-center mb-4">
+          <h1 className="text-2xl font-bold text-blue-50">
+            Data Fetching from the food DB API
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-6">
+          {meals.map((meal, index) => {
+            return <Card key={index} meal={meal} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Card({ meal }) {
+  return (
+    <div className="bg-white rounded-xl shadow-md overflow-hidden">
+      <div>
+        <img
+          src={meal.strMealThumb}
+          alt="meal img"
+          className="w-full object-cover h-48 md:h-56"
+        />
+        <div>
+          <h2 className="font-semibold text-lg m-2">{meal.strMeal}</h2>
+          <p className="text-gray-600 px-2">{meal.strInstructions.slice(0,100)} ....</p>
+        </div>
+        <div>
+          <button className="px-2 py-1 font-semibold text-blue-400 hover:underline">Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
+}
