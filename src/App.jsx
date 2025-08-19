0c15e117d929464099f224a820bf30de313d205b
RaiyanTanzim <writetoraiyan@gmail.com>
2025-08-19 15:51:15 +0600

    updated-commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import "./App.css";
 
 function App() {
   const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
       .then((response) => response.json())
       .then((data) => {
         setMeals(data.meals);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err.message);
+        setLoading(false);
       });
   }, []);
   return (
@@ -20,6 +28,14 @@ function App() {
           <h1 className="text-2xl font-bold text-blue-50">
             Data Fetching from the food DB API
           </h1>
+        </div>
+        <div className="text-center">
+          {
+            loading && <p className="font-semibold text-lg">Loading... Please Wait.</p>
+          }
+                {
+            error && <p className="text-sm text-red-500">Something Went Wrong!</p>
+          }
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-6">
           {meals.map((meal, index) => {
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Card({ meal }) {
   return (
     <div className="bg-white rounded-xl shadow-md overflow-hidden">
@@ -7,14 +5,18 @@ export default function Card({ meal }) {
         <img
           src={meal.strMealThumb}
           alt="meal img"
-          className="w-full object-cover h-48 md:h-56"
+          className="w-full object-cover h-48 md:h-56 cursor-pointer hover:scale-105 transition-all duration-200"
         />
         <div>
           <h2 className="font-semibold text-lg m-2">{meal.strMeal}</h2>
-          <p className="text-gray-600 px-2">{meal.strInstructions.slice(0,100)} ....</p>
+          <p className="text-gray-600 px-2">
+            {meal.strInstructions.slice(0, 100)} ....
+          </p>
         </div>
         <div>
-          <button className="px-2 py-1 font-semibold text-blue-400 hover:underline">Learn More</button>
+          <button className="px-2 py-1 font-semibold text-blue-400 hover:underline cursor-pointer">
+            Learn More
+          </button>
         </div>
       </div>
     </div>
